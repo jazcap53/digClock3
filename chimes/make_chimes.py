661552ce7803fa3086e2494b8800174e87734fed
Andrew Jarcho <andrew.jarcho@gmail.com>
2016-08-27 04:40:42 -0400

    Update make_chimes.py to conform to PEP-8

--- chimes/make_chimes.py
+++ chimes/make_chimes.py
@@ -1,11 +1,13 @@
 import pysynth
 
-set1 = [['g#3',4],['f#3',4],['e3',4],['b2',2],]
-set2 = [['e3',4],['g#3',4],['f#3',4],['b2',2],]
-set3 = [['e3',4],['f#3',4],['g#3',4],['e3',2],]
-set4 = [['g#3',4],['e3',4],['f#3',4],['b2',2],]
-set5 = [['b2',4],['f#3',4],['g#3',4],['e3',2],]
-rest = [['r',4]]
+""" Create or re-create the audio files if necessary """
+
+set1 = [['g#3', 4], ['f#3', 4], ['e3', 4], ['b2', 2], ]
+set2 = [['e3', 4], ['g#3', 4], ['f#3', 4], ['b2', 2], ]
+set3 = [['e3', 4], ['f#3', 4], ['g#3', 4], ['e3', 2], ]
+set4 = [['g#3', 4], ['e3', 4], ['f#3', 4], ['b2', 2], ]
+set5 = [['b2', 4], ['f#3', 4], ['g#3', 4], ['e3', 2], ]
+rest = [['r', 4]]
 
 quarter1 = set1
 quarter2 = set2 + set3
@@ -16,35 +18,37 @@ print
 print "Creating chime files... (this might take about a minute)"
 print
 
-# create the hour chimes: h1mono.wav, h2mono.wav, ...
+# create the hourly bells: h1mono.wav, h2mono.wav, ...
 on_hour = []
-for i in range(1,10):
-    on_hour.append(['e2',-2])
-    pysynth.make_wav(on_hour, bpm = 60, fn = "h0" + str(i) + "mono.wav")
-for j in range(10,13):
-    on_hour.append(['e2',-2])
-    pysynth.make_wav(on_hour, bpm = 60, fn= "h" + str(j) + "mono.wav")
+for i in range(1, 10):
+    on_hour.append(['e2', -2])
+    pysynth.make_wav(on_hour, bpm=60, fn="h0" + str(i) + "mono.wav")
+for j in range(10, 13):
+    on_hour.append(['e2', -2])
+    pysynth.make_wav(on_hour, bpm=60, fn="h" + str(j) + "mono.wav")
 
-# quarter hour
-pysynth.make_wav(quarter1, bpm = 60, fn = "q1mono.wav")
+# quarter hour chimes
+pysynth.make_wav(quarter1, bpm=60, fn="q1mono.wav")
 
-# half hour
-pysynth.make_wav(quarter2, bpm = 60, fn = "q2mono.wav")
+# half hour chimes
+pysynth.make_wav(quarter2, bpm=60, fn="q2mono.wav")
 
-# three-quarter hour
-pysynth.make_wav(quarter3, bpm = 60, fn = "q3mono.wav")
+# three-quarter hour chimes
+pysynth.make_wav(quarter3, bpm=60, fn="q3mono.wav")
 
-# full hour
-pysynth.make_wav(quarter4, bpm = 60, fn = "q4mono.wav")
+# full hour chimes
+pysynth.make_wav(quarter4, bpm=60, fn="q4mono.wav")
+
 '''
-# make the quarters in stereo
-for i in range(1,5):
+# make the quarterly chimes in stereo
+for i in range(1, 5):
     pysynth.mix_files("q" + str(i) + "mono.wav", "q" + str(i) + "mono.wav",\
             "q" + str(i) + "stereo.wav")
 
-# make the hours in stereo
+# make the hourly bells in stereo
 for i in range(1, 13):
     pysynth.mix_files("h" + str(i) + "mono.wav", "h" + str(i) + "mono.wav",\
             "h" + str(i) + "stereo.wav")
 '''
+
 print "Finished"
