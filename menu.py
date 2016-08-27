ef936d8a9ca0d6f599b5a8263fcc0ea7ccc322ef
Andrew Jarcho <andrew.jarcho@gmail.com>
2016-08-27 08:33:26 -0400

    Update comments for menu.py; one cosmetic change to code in check_bkgnd_ne_fgnd()

--- menu.py
+++ menu.py
@@ -1,9 +1,14 @@
+from __future__ import print_function
 import os
 import time
 import menu_data
 
 
 class Menu:
+    """
+    Class provided: an interactive menu presented to user
+    Instantiated by global function cycle_menus()
+    """
 
     header = 'Welcome to DigClock'
     message = 'Please select a '
@@ -17,13 +22,19 @@ class Menu:
         self.chosen = chosen[:]
 
     def run(self):
+        """
+        Calls subroutines that:
+            read and display the current menu
+            get and validate the user response
+        Called by: global function cycle_menus()
+        """
         self.read()
         self.display()
-        selection = None  # present to make PyCharm happy
-        while True:
+        selection = None  # to make PyCharm happy
+        while True:  # loop until user makes a valid selection
             selection = self.get_selection()
             test_1 = self.validate_selection(selection, len(self.entries))
-            test_2 = True  # may be set to False
+            test_2 = True  # may be set to False two lines down
             if self.description == 'background color' and test_1:
                 test_2 = self.check_bkgnd_ne_fgnd(selection)
                 if not test_2:  # bkgrnd and text colors are the same
@@ -36,6 +47,10 @@ class Menu:
         self.update_chosen(selection)
 
     def read(self):
+        """
+        Reads the menu data
+        Called by: self.run()
+        """
         enum_menu_data = enumerate(self.source)
         for ix, item in enum_menu_data:
             if ix == 0:
@@ -50,30 +65,37 @@ class Menu:
 
     def display(self):
         """
-        Present a menu to the user.
-        :return: None
+        Present current menu to the user.
+        Called by: self.run()
         """
         os.system('clear')
         print(Menu.header + '\n')
         print(Menu.message + self.description + ':\n')
         for item in self.entries[1:]:
             print('{:2}) {:10}'.format(item[0], item[1]))
-        # print('')
+        print()
         for item in self.chosen:  # self.chosen is empty for first menu
             print('Your {}: {}'.format(item[0], item[2]))
 
     def get_selection(self):
+        """
+        Get and return the user selection.
+        Called by: self.run()
+        """
         sel = raw_input('\n\n' + self.footer + ' ')
         return sel
 
     @staticmethod
     def validate_selection(sel, menu_len):
         """
+        Checks that user input represents an integer in the correct
+        range, or is a null string.
         :param sel: the raw user input
         :param menu_len: a two-item menu will have entries 0, 1, 2
-               where entry 0 holds None
+               where entry 0 holds null strings to mimic 1-indexing
         :return: True on good input
                  False otherwise
+        Called by: self.run()
         """
         ret = False
         sel = sel.strip()
@@ -82,18 +104,30 @@ class Menu:
                 sel_as_int = int(sel)
                 if 0 < sel_as_int < menu_len:
                     ret = True
-            except ValueError:
+            except ValueError:  # input is non-numeric
                 pass  # ret is already False
         else:
             ret = True  # empty string is a valid input
         return ret
 
     def check_bkgnd_ne_fgnd(self, sel):
+        """
+        Checks that the user's choice of text color is not the same
+        as their choice of background color.
+        :param sel: The user's choice, which may be a null string but is
+            otherwise a valid integer (as string)
+        :return: True if text color and background color are different
+                 False if they are the same
+        Called by: self.run()
+        """
         if not sel:
             bkgnd_val = self.default[1]
         else:
             sel_as_int = int(sel)  # sel has already been strip()ped
+            # self.entries[sel_as_int][1] may be e.g.,
+            # ('1', 'BLACK (*)', '\x1b[40m')
             bkgnd_val = self.entries[sel_as_int][1].rstrip(' ()*')
+        # self.chosen[0]: stored selection from previous menu
         fgnd_val = self.chosen[0][2]
         if fgnd_val == bkgnd_val:
             return False
@@ -101,12 +135,22 @@ class Menu:
 
     @staticmethod
     def print_err_msg(err_msg):
-        print('\033[41m')
+        """
+        Called by: self.run()
+        """
+        print('\033[41m')  # red background
         print(err_msg)
         time.sleep(2)
-        print('\033[40m')
+        print('\033[40m')  # black background
 
     def update_chosen(self, sel):
+        """
+        Stores option selected by user
+        :param sel: user choice as a string (may be null)
+        Called by: self.run()
+        """
+        # description is a *string* describing the current menu
+        # e.g, 'text color'
         description_as_list = [self.description]
         if sel:
             list_to_append = description_as_list
@@ -117,19 +161,23 @@ class Menu:
                     list_to_append += [short_entry]
                 else:
                     list_to_append += [entry]
-            self.chosen.append(list_to_append)
         else:
             list_to_append = description_as_list + self.default
-            self.chosen.append(list_to_append)
+        self.chosen.append(list_to_append)
 
 
 def cycle_menus():
-    """ Call run() for each menu """
+    """
+    Calls run() for each menu
+    :return: the collected user choices for each menu run
+    Called by: client get_time.run_clock()
+    """
     global_chosen = []  # holds selections from all menus
     for m in [menu_data.first, menu_data.second, menu_data.third,
               menu_data.fourth]:
         this_menu = Menu(m, global_chosen)
-        # display menu, get and validate selection, update saved choices
+        # read and display menu, get and validate selection,
+        # update saved choices
         this_menu.run()
         global_chosen.append(this_menu.chosen[-1])
     os.system('clear')
